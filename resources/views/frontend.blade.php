65ebc71fd9072685d151f3761ea35e99cd5bfba3
felixprajna-santoso <felikspraj@gmail.com>
2025-05-21 22:40:00 +0700

    PR Pertemuan 11

---
+++ resources/views/frontend.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Halaman Pertemuan</title>
+</head>
+<body>
+    <h1>PWEB SMT 4</h1>
+    <ul>
+         <li><a href="{{ url('/bootstrapkode2_5026231027') }}">bootstrapkode2_5026231027</a></li>
+         <li><a href="{{ url('boxmodel') }}">boxmodel</a></li>
+         <li><a href="{{ url('/imagefilter') }}">imagefilter</a></li>
+         <li><a href="{{ url('/js1') }}">js1</a></li>
+         <li><a href="{{ url('/Latihan17april') }}">Latihan17april</a></li>
+         <li><a href="{{ url('/linktree') }}">linktree</a></li>
+         <li><a href="{{ url('/index') }}">index</a></li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +11,45 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend');
+});
+
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/bootstrapkode2_5026231027', function () {
+    return view('bootstrapkode2_5026231027');
+});
+
+Route::get('/boxmodel', function () {
+    return view('boxmodel');
+});
+
+Route::get('/imagefilter', function () {
+    return view('imagefilter');
+});
+
+Route::get('/js1', function () {
+    return view('js1');
+});
+
+Route::get('/Latihan17april', function () {
+    return view('Latihan17april');
+});
+
+Route::get('/linktree', function () {
+    return view('linktree');
+});
+
+Route::get('/index', function () {
+    return view('index');
 });
